41be5ecf11c2b1f717bd8eb1f307b3f360d5e618
Arnold Su <arnoldtingsu@gmail.com>
2025-12-17 22:14:39 -0800

    added crew page

--- fish4hoes/views.py
+++ fish4hoes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def fishington_home(request):
   return render(request, "website/fishington.html")
+
+def crewLive(request):
+  return render(request, "website/crewLive.html")
